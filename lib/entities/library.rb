98faa1eeba94e2a7c3462b6728e528ffdb73b0a7
Ed de Almeida <edvaldoajunior@gmail.com>
2016-11-15 10:24:21 -0200

    'libray list' implemented

--- lib/entities/library.rb
+++ lib/entities/library.rb
@@ -2,6 +2,45 @@ module EmeraldFW
 
   class Library < EmeraldFW::Entity
 
+  	def initialize(args,opts)
+  	  @project = EmeraldFW::Project.new(args,opts)  
+  	  EmeraldFW.exit_error(203) if @project.current_project.empty?		
+  	  @valid_options = []
+  	  super(args,opts)
+  	end
+
+  	def entity_type
+  	  :library
+  	end
+
+  	def valid_commands
+  	  [ :list, :create, :remove ]
+  	end
+
+  	def libraries_base_dir
+  	  "#{ENV['HOME']}/emeraldfw/#{@project.current_project}/web/libraries"
+  	end
+
+  	# 
+  	# Command's implementations
+  	# 
+
+  	def list
+  	  print "Project "
+  	  print @project.current_project.colorize(:green)
+  	  puts "'s libraries: "
+  	  libraries = Dir.entries(libraries_base_dir).reject { |d| (d == '.') || (d == '..') }.sort
+  	  libraries.each do |lib|
+  	  	puts  "   #{lib}".colorize(:light_green)
+  	  end
+  	end
+
+  	def create
+  	end
+
+  	def remove
+  	end 	
+
   end
 
 end
