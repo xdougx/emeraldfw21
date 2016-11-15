e42f63b0c7b3793d778976e06aa0b83afffe7808
Ed de Almeida <edvaldoajunior@gmail.com>
2016-11-15 03:48:30 -0200

    'resource list' implemented

--- lib/entities/resource.rb
+++ lib/entities/resource.rb
@@ -26,11 +26,21 @@ module EmeraldFW
   	# 
 
   	def list
+  	  print "Project: "
+  	  puts @project.current_project.colorize(:green)
   	  rlibs = Dir.entries(resources_base_dir).reject{ |d| (d == '.') || (d == '..') }.sort
-  	  puts rlibs
+  	  rlibs.each do |lib| 
+  	  	print "   Resource library: "
+  	  	puts  lib.colorize(:light_green)
+  	  	assets = Dir.entries("#{resources_base_dir}/#{lib}").reject{ |d| (d == '.') || (d == '..') }.sort
+  	  	assets.each do |asset|
+  	  	  puts "      #{asset}"
+  	  	end
+  	  end
   	end
 
   	def create
+  	  
   	end
 
   	def remove
